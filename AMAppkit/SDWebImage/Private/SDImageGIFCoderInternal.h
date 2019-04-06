738de8e081a9bc115fda3c11d96469010fc8c19e
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2019-04-06 20:41:29 +0300

    updated sd web image

--- AMAppkit/SDWebImage/Private/SDImageGIFCoderInternal.h
+++ AMAppkit/SDWebImage/Private/SDImageGIFCoderInternal.h
@@ -2,17 +2,15 @@
  * This file is part of the SDWebImage package.
  * (c) Olivier Poitrey <rs@dailymotion.com>
  *
- * Created by james <https://github.com/mystcolor> on 9/28/11.
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-#import <Foundation/Foundation.h>
 #import "SDWebImageCompat.h"
+#import "SDImageGIFCoder.h"
 
-@interface UIImage (ForceDecode)
+@interface SDImageGIFCoder ()
 
-+ (UIImage *)decodedImageWithImage:(UIImage *)image;
+- (float)sd_frameDurationAtIndex:(NSUInteger)index source:(nonnull CGImageSourceRef)source;
 
 @end
